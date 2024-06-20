487895b864974e8c6435c5667e4bf21bf0af2759
Andrey Helldar <helldar@dragon-code.pro>
2024-06-21 02:42:10 +0300

    Added lang folder pre-cleaning

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,8 @@
 <?php
 
+use DragonCode\Support\Facades\Filesystem\File;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(Tests\TestCase::class, RefreshDatabase::class)->in(__DIR__);
+uses(Tests\TestCase::class, RefreshDatabase::class)->beforeEach(
+    fn () => File::ensureDelete(lang_path('en.json'))
+)->in(__DIR__);
