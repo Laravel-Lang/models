8ad202a6b869b72d7443385956bcda3deaa76042
Andrey Helldar <helldar@dragon-code.pro>
2024-09-27 15:23:59 +0300

    The `translationModelName` method has been made public

--- src/Concerns/HasNames.php
+++ src/Concerns/HasNames.php
@@ -8,7 +8,7 @@ use LaravelLang\Config\Facades\Config;
 
 trait HasNames
 {
-    protected function translationModelName(): string
+    public function translationModelName(): string
     {
         return static::class . Config::shared()->models->suffix;
     }
